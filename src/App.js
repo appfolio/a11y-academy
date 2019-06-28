98ec0ae4c61c0910aa8fb7d651d08607631a7dd0
Darren Eng <darren.eng@appfolio.com>
2019-06-28 16:50:39 -0700

    de - replace css with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -19,7 +16,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Container>
   );
 }
 
