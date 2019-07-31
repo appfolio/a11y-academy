067fee9f89c0c92e2ffc737a89cce48698711e70
Darren Eng <darrenle9@gmail.com>
2019-07-30 22:08:55 -0700

    de - add routing test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,47 @@
-import { cleanup, render } from '@testing-library/react';
-import axe from 'axe-core';
-import React from 'react';
-import App from './App';
+import { cleanup, render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import {
+  createHistory,
+  createMemorySource,
+  LocationProvider
+} from "@reach/router";
+import axe from "axe-core";
+import React from "react";
+import App from "./App";
 
-describe('App', () => {
+function renderWithRouter(
+  ui,
+  { route = "/", history = createHistory(createMemorySource(route)) } = {}
+) {
+  return {
+    ...render(<LocationProvider history={history}>{ui}</LocationProvider>),
+    history
+  };
+}
+
+describe("App", () => {
   afterEach(cleanup);
 
-  it('doesnt have any accessibility violations', async () => {
+  it("doesnt have any accessibility violations", async () => {
     const { container } = render(<App />);
     const { violations } = await axe.run(container);
     expect(violations).toEqual([]);
+  });
+
+  it("routes everywhere correctly", async () => {
+    const {
+      container,
+      getByRole,
+      history: { navigate }
+    } = renderWithRouter(<App />);
+
+    await navigate("/");
+    expect(container.innerHTML).toMatch("Today We Learned");
+
+    await navigate("/faqs");
+    expect(container.innerHTML).toMatch("Frequently Asked Questions");
+
+    await navigate("/entries/new");
+    expect(getByRole("heading")).toHaveTextContent("New TWL Entry");
   });
 });
