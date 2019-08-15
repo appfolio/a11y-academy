0eab8e7473054dde15d1d8de5dae71b3e2dbc998
Darren Eng <darrenle9@gmail.com>
2019-08-14 23:03:00 -0700

    de - add a banner image to the index page

--- src/IndexPage.js
+++ src/IndexPage.js
@@ -1,8 +1,19 @@
 import { Link } from "@reach/router";
 import React from "react";
-import { Button } from "reactstrap";
+import { Button, Jumbotron } from "reactstrap";
 import TILCard from "./TILCard";
 
+function BannerImage() {
+  return (
+    <Jumbotron>
+      <img
+        src="https://www.nps.gov/common/uploads/banner_image/pwr/homepage/F836E601-1DD8-B71B-0BC2936CDBC2FC26.jpg?width=2400&height=700&mode=crop&quality=90"
+        alt="Joshua Tree"
+        className="img-fluid"
+      />
+    </Jumbotron>
+  );
+}
 export default function IndexPage({ entries }) {
   React.useEffect(() => {
     document.title = "Today We Learned";
@@ -12,6 +23,7 @@ export default function IndexPage({ entries }) {
     <>
       <header>
         <h1 data-testid="page-heading">Today We Learned</h1>
+        <BannerImage />
         <p>
           A bunch of write ups about something a team learned. Failure is ok as
           long as we learn from it!
